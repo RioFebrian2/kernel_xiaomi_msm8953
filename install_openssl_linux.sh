cb48d8b4f876520631a3d774b7e2fbe6a518d81f
Rio Febrian <37467302+RioFebrian2@users.noreply.github.com>
2019-04-13 03:47:23 +0700

    Instal Openssl
    Fix Fatal error Openssl compile kernel source 4.x

---
+++ install_openssl_linux.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Fix openssl compile kernel 4.x
+
+"sudo apt update"
+"sudo apt-get install openssl"
+"sudo apt-get install -y chrpath gawk texinfo libsdl1.2-dev whiptail diffstat cpio libssl-dev"
